74a078b8a38c183d30147cd5e66183d5c8cdebbe
zdvman <zuyevdmytro@gmail.com>
2025-02-20 11:42:12 +0000

    11. CORE: Sort articles

--- src/pages/TopicsPage.jsx
+++ src/pages/TopicsPage.jsx
@@ -4,6 +4,7 @@ import Loading from '../components/Loading';
 import { NavLink } from 'react-router-dom';
 import { ChevronLeftIcon } from '@heroicons/react/16/solid';
 import { Heading } from '../components/catalyst-ui-kit/heading';
+import { Text } from '../components/catalyst-ui-kit/text';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
@@ -33,7 +34,7 @@ export default function TopicsPage() {
         </NavLink>
       </div>
       <Heading className='my-6'>Topics</Heading>
-      <div className='divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow-sm sm:grid sm:grid-cols-2 sm:gap-px sm:divide-y-0'>
+      <div className='grid sm:grid-cols-2 gap-2'>
         {topics.map((topic, topicIdx) => (
           <div
             key={topic?.slug + topic?.description}
@@ -46,25 +47,29 @@ export default function TopicsPage() {
               topicIdx === topics.length - 1
                 ? 'rounded-br-lg rounded-bl-lg sm:rounded-bl-none'
                 : '',
-              'group relative bg-white p-6 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-inset'
+              'group relative bg-gray-900 dark:bg-gray-800 p-6 border border-gray-700 rounded-lg transition-all duration-300'
             )}
           >
             <div className='mt-8'>
-              <h3 className='text-base font-semibold text-gray-900'>
+              <Heading
+                level={3}
+                className='text-base font-semibold text-gray-900'
+              >
                 <NavLink
                   to={`/articles/${topic?.slug}`}
-                  className='focus:outline-hidden'
+                  className='focus:outline-none hover:text-indigo-400 transition-colors duration-200'
                 >
                   {/* Extend touch target to entire panel */}
                   <span aria-hidden='true' className='absolute inset-0' />
-                  {topic?.slug}
+                  Topic:{' '}
+                  {`"${topic?.slug[0].toUpperCase() + topic?.slug.slice(1)}"`}
                 </NavLink>
-              </h3>
-              <p className='mt-2 text-sm text-gray-500'>{topic.description}</p>
+              </Heading>
+              <Text className='mt-2'>{topic.description}</Text>
             </div>
             <span
               aria-hidden='true'
-              className='pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400'
+              className='pointer-events-none absolute top-6 right-6 text-gray-500 group-hover:text-indigo-400 transition-colors duration-200'
             >
               <svg fill='currentColor' viewBox='0 0 24 24' className='size-6'>
                 <path d='M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z' />
